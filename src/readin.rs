360b3cbb49837ab58558d71b18eec3705bab9b36
Ramenspazz <kirisame.ramen@gmail.com>
2023-06-01 23:02:26 -0700

    Add files via upload

--- src/readin.rs
+++ src/readin.rs
@@ -6,50 +6,48 @@ pub fn readin(mut file: &mut std::fs::File) -> Option<f64> {
     let mut num_char_before_decimal: u16 = 0u16;
     let mut num_char_after_decimal: u16 = 0u16;
     let mut reading_dec: bool = false;
-    let mut num_arr: [u32; 65535] = [0; 65535]; // max size is 65534, which is the maximum
+    let mut num_arr: [u32; 65535] = [0; 65535];
 
     while let Ok(read_bytes) = std::io::Read::read(&mut file, &mut u8_buf) {
         if read_bytes == 0 {
             break;
         }
-        // split the string into slices at spaces, setting each part to a number
+
         match u8_buf.get(0) {
             Some(char_val) => {
                 if *char_val == 32u8 || *char_val == 10u8 {
-                    // If the character is a space or newline, stop adding digits and compute the final number
+                // If the character is a space or newline, stop adding digits and compute the final number
                     let mut final_number: f64 = 0.0;
                     let final_num = num_char_before_decimal + num_char_after_decimal;
-                    for arr_index in (0..final_num).rev() {
+
+                    for arr_index in 0..final_num {
+                        let current_digit = *num_arr.get(arr_index as usize).unwrap() as f64;
+
                         if arr_index < num_char_before_decimal {
-                            // Processing digits before the decimal point
-                            final_number +=
-                                *num_arr.get((num_char_before_decimal - arr_index - 1) as usize)
-                                    .unwrap() as f64
-                                    * 10f64.powf(arr_index as f64);
+                            final_number = final_number * 10f64 + current_digit;
                         } else {
-                            // Processing digits after the decimal point
-                            final_number +=
-                                *num_arr.get((arr_index - num_char_before_decimal + 1) as usize)
-                                    .unwrap() as f64
-                                    / 10f64.powf((arr_index - num_char_before_decimal + 1) as f64);
+                            let decimal_place = (arr_index - num_char_before_decimal + 1) as f64;
+                            final_number += current_digit / 10f64.powf(decimal_place);
                         }
                     }
+
                     return Some(final_number);
                 } else if *char_val == 46u8 {
-                    // The character is a decimal point, mark that we are now processing decimals
+                // The character is a decimal point, mark that we are now processing decimals
                     reading_dec = true;
                     continue;
                 } else if *char_val >= 48u8 && *char_val <= 57u8 {
-                    // The character is a number and we can work with it
-                    if reading_dec == true {
+                // The character is an ascii number and we can work with it
+                    let current_digit = (*char_val - 48) as u32;
+
+                    if reading_dec {
                         num_char_after_decimal += 1;
                     } else {
                         num_char_before_decimal += 1;
                     }
+
                     match num_arr.get_mut((num_char_before_decimal + num_char_after_decimal - 1) as usize) {
-                        Some(value) => {
-                            *value = (*char_val - 48) as u32;
-                        }
+                        Some(value) => *value = current_digit,
                         None => panic!(),
                     }
                 }
@@ -57,20 +55,6 @@ pub fn readin(mut file: &mut std::fs::File) -> Option<f64> {
             None => return None,
         }
     }
-    return None;
-}
 
-// // coerce to a &str : safety
-// let s: Result<&str, str::Utf8Error> = unsafe {
-//     // First, we build a &[u8]...
-//     let slice = std::slice::from_raw_parts(u8_buf.as_mut_ptr(), read_bytes);
-//     // ... and then convert that slice into a string slice
-//     std::str::from_utf8(slice)
-// };
-// match s {
-//     Ok(value) => print!("{}", &String::from(value)),
-//     Err(_) => panic!(),
-// }
-// std::io::stdout()
-//     .flush()
-//     .unwrap();
+    None
+}
